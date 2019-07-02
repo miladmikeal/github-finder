cb1d3a6f64d11cab2d3e5a021c03988328f58e41
Milad Mikeal <miladmikeal@gmail.com>
2019-07-01 19:56:53 -0700

    Add clear button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
   }
 
   searchUsers = async (text) => {
-    this.setState({ loading: true })
     try {
       this.setState({ loading: true })
       const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
@@ -22,13 +21,20 @@ class App extends Component {
     }
   }
 
+  clearUsers = () => {
+    this.setState({
+      users: [],
+      loading: false
+    })
+  }
+
   render() {
     const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
           <Users users={users} loading={loading} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
   }
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   }
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -19,12 +21,16 @@ class Search extends Component {
   }
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} action="" className="form">
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+        )}
       </div>
     )
   }
