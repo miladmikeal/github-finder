a9dccbb279f51e6bc88de9d397c0fd13b176b021
Milad Mikeal <miladmikeal@gmail.com>
2019-07-07 15:15:43 -0700

    Add context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,15 +1,21 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
     // eslint-disable-next-line
   }, []);
+
+
 
   const {
     name,
@@ -87,14 +93,6 @@ const User = ({ user, loading, repos, getUser, getUserRepos, match }) => {
       </Fragment>
     )
   }
-}
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
